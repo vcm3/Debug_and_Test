a831db66807344b7918e530abdf06e9041cac2dc
fmachadopiriz <fmachadopiriz@outlook.com>
2019-09-02 19:13:59 -0300

    Commit inicial

---
+++ README.md
@@ -0,0 +1,4 @@
+# Universidad Católica del Uruguay
+## Facultad de Ingeniería y Tecnologías
+### Programación II
+Código para la clase de debug and test
---
+++ test/Library.Test/PersonTests.cs
@@ -0,0 +1,19 @@
+using NUnit.Framework;
+
+namespace Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Insertá tu código de inicialización aquí
+        }
+
+        [Test]
+        public void Test1() // Cambiá el nombre para indicar qué estás probando
+        {
+            // Insertá tu código  de pruebaaquí
+        }
+    }
+}

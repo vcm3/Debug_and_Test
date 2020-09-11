773eaaa591b157397cd800ba2f2efaaed181da3b
Valentina Cabrera <valentina.cabreram@correo.ucu.edu.uy>
2020-09-11 19:56:53 -0300

    .

---
+++ TreasureHunt/src/Program/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Vigenere.Library;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            VigenereCode v = new VigenereCode("ucu", 8);
+            Console.WriteLine(v.Decrypt("ISVJED"));
+            Console.WriteLine(v.Decrypt("WMWFKN"));
+            Console.WriteLine(v.Decrypt("TORQ"));
+            Console.WriteLine(v.Decrypt("JSFFOP"));
+        }
+    }
+}
